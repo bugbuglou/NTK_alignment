ceab6f824ee6385e65e95b1003c72d3375d62416
bugbuglou <58448037+bugbuglou@users.noreply.github.com>
2021-09-29 03:30:34 +0800

    Update GOFE.py

--- GOFE.py
+++ GOFE.py
@@ -227,7 +227,7 @@ def compute_hessian_spectrum(model, loader, path, cal_target = False):
 
     return density_eigen, density_weight
 
-def gofe_corr(model, output_fn, loader, eigvals, eigvecs, w, K_prev_generator, n_output, device):
+def gofe_corr(model, output_fn, loader, eigvals, eigvecs, w, K_prev_generator, n_output, device, centering):
     # compute correlation between delta psi and HPsiwwT
     
     H_w = torch.matmul(torch.from_numpy(eigvecs.copy()).transpose(1,0), torch.matmul(torch.diag(torch.tensor(eigvals.copy(), dtype = torch.float32)), torch.from_numpy(eigvecs.copy())))
@@ -1270,8 +1270,8 @@ def process(index):
                                                                   args['num_eigenthings'])
                 # print(to_log['eigenvecs'].shape[1] == sum(widths))
 
-                K_prev_generator_train, to_log['corr_gofe_train'] = gofe_corr(model, output_fn, dataloaders['micro_train'], to_log['eigenvals'], to_log['eigenvecs'], to_log['w_train'], K_prev_generator_train, n_output = 10, device = device)
-                K_prev_generator_test, to_log['corr_gofe_test'] = gofe_corr(model, output_fn, dataloaders['micro_test'], to_log['eigenvals_test'], to_log['eigenvecs_test'], to_log['w_test'], K_prev_generator_test, n_output = 10, device = device)
+                K_prev_generator_train, to_log['corr_gofe_train'] = gofe_corr(model, output_fn, dataloaders['micro_train'], to_log['eigenvals'], to_log['eigenvecs'], to_log['w_train'], K_prev_generator_train, n_output = 10, device = device, centering = True)
+                K_prev_generator_test, to_log['corr_gofe_test'] = gofe_corr(model, output_fn, dataloaders['micro_test'], to_log['eigenvals_test'], to_log['eigenvecs_test'], to_log['w_test'], K_prev_generator_test, n_output = 10, device = device, centering = True)
                 
 
                 num = args['num_eigenthings']
